939451fdc25d8609be9f464c31a09938508a3d56
Ben Bryant <brbryant2639@gmail.com>
2020-03-04 09:15:03 -0600

    initial commit

---
+++ chirper/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1 className='jumbotron'>Chirper</h1>
+      <p className='rounded bg-light'>Message</p>
+    </React.Fragment>
+  );
+}
+
+export default App;
